c4900cd19d57a210f77700c34c5cc130b31617f8
Efraín García Valencia <99913200+EfrainGarciaV@users.noreply.github.com>
2022-02-27 22:55:23 -0500

    v1.1

--- src/main/java/ClasePrincipal.java
+++ src/main/java/ClasePrincipal.java
@@ -1,3 +1,11 @@
+/*
+ Reto 9
+ Hecho por:
+ Efraín García(CC.1001370984)
+ Samuel Acevedo(CC.1001016099)
+*/
+
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,36 +14,54 @@ import java.util.List;
 
 public class ClasePrincipal {
     public static void main(String[] args) {
+
         String ruta1 = "src/main/BTC-USD.csv";
         String ruta2 = "src/main/Punto1.txt";
       //String ruta3 = "src/main/Punto2.txt";
+
         try {
             escribirLineaPorLinea(ruta1 , ruta2);
+            System.out.println("Media: "+media(ruta1));
+            System.out.println("Desviación estándar: "+desviacionEstandar(ruta1));
         } catch (IOException e) {
             System.out.println("Error" + e.getMessage());
         }
     }
 
-    public static void escribirLineaPorLinea(String rutaLeer , String rutaEscribir ) throws IOException {
+    public static void escribirLineaPorLinea(String rutaLeer , String rutaEscribir ) throws IOException , NullPointerException{
 
-        Path miRutaLeer = Paths.get(rutaLeer);
         Path miRutaEscribir = Paths.get(rutaEscribir);
-        List<String> lineasArchivoLeer;
         StringBuilder cadena = new StringBuilder();
-        String[] palabras;
         String miConceptoCierre;
+        String[] palabras;
+        int i = 1;
+        while (LeerLineaPorLinea(rutaLeer, i) != null) {
 
-
-            lineasArchivoLeer = Files.readAllLines(miRutaLeer);
-
-            for (int i = 1, lineasArchivoLeerSize = lineasArchivoLeer.size(); i < lineasArchivoLeerSize; i++) {
-                String lineaActual = lineasArchivoLeer.get(i);
-                palabras = lineaActual.split(",");
-                miConceptoCierre = conceptoCierre(palabras[4]);
-                cadena.append("Fecha: ").append(palabras[0]).append("\t").append("Concepto de cierre: ").append(miConceptoCierre).append("\n");
-            }
+            palabras = LeerLineaPorLinea(rutaLeer, i);
+            assert palabras != null;
+            miConceptoCierre = conceptoCierre(palabras[4]);
+            cadena.append("Fecha: ").append(palabras[0]).append("\t").append("Concepto de cierre: ").append(miConceptoCierre).append("\n");
+            i++;
+        }
 
         Files.writeString(miRutaEscribir, cadena.toString());
+    }
+
+    public static String[] LeerLineaPorLinea(String rutaALeer , int x) throws IOException {
+
+        Path miRutaLeer = Paths.get(rutaALeer);
+        List<String> lineasArchivoLeer;
+        String[] palabrasSeparadas;
+        lineasArchivoLeer = Files.readAllLines(miRutaLeer);
+        try {
+            String lineaActual = lineasArchivoLeer.get(x);
+            palabrasSeparadas = lineaActual.split(",");
+        } catch (IndexOutOfBoundsException k) {
+            return null;
+        }
+
+        return palabrasSeparadas;
+
     }
 
     public static String conceptoCierre(String c) {
@@ -57,28 +83,80 @@ public class ClasePrincipal {
         else return "MUY ALTO";
     }
 
-    public static float media(String[] datos){   //Itera sobre el Array palabras, tomo los elementos de la posicion 1 y calculo la media
+    public static float media(String rutaLeer) throws IOException {
+        int i = 1;
         float sumaElementos=0;
-        for (String dato : datos) {      //La lógica es como en el array palabras usado para el conceptoCierre
-            sumaElementos += Float.parseFloat(dato);
+        String[] palabras;
+        while (LeerLineaPorLinea(rutaLeer, i) != null) {
+            palabras = LeerLineaPorLinea(rutaLeer , i);
+            assert palabras != null;
+            sumaElementos += Float.parseFloat(palabras[1]);
+            i++;
         }
-        return sumaElementos/ datos.length;
+        return sumaElementos/ i-1;
     }
 
-    public static double desviacionEstandar(String[] datos){
-        float mediaDeLosDatos = media(datos);
+    public static double desviacionEstandar(String rutaLeer) throws IOException {
+        float mediaDeLosDatos = media(rutaLeer);
         float sumatoriaDistanciasCuadradas=0;
         float distanciaConLaMedia;
-        /*El array datos usado para los metodos media y varianza contiene los valores de apertura
-            de cada día, navegando en el solo se encuentran numeros en Strings
 
-            Por lo tanto, cuando se lean los datos hay que crear un array aparte con los datos
-            de la posicion 1 del vector palabras para cada línea(Aún falta hacerlo)
-         */
-        for(String valor:datos){
-            distanciaConLaMedia = Float.parseFloat(valor) - mediaDeLosDatos;
+        int i = 1;
+        String[] palabras;
+        while (LeerLineaPorLinea(rutaLeer, i) != null) {
+            palabras = LeerLineaPorLinea(rutaLeer , i);
+            assert palabras != null;
+            distanciaConLaMedia = Float.parseFloat(palabras[1]) - mediaDeLosDatos;
             sumatoriaDistanciasCuadradas += Math.pow(distanciaConLaMedia,2);
+            i++;
         }
-        return Math.sqrt(sumatoriaDistanciasCuadradas/datos.length);
+        return Math.sqrt(sumatoriaDistanciasCuadradas/i-1);
+    }
+
+    public static String[] valorMax(List<String> lineasConPrecios){
+        int i=1;
+        int j=i+1;
+        double datoMax = 0;
+        String fechaDato = null;
+        while (i < lineasConPrecios.size() && j < lineasConPrecios.size()-1) {
+            String lineaActual = lineasConPrecios.get(i);
+            String lineaSiguiente = lineasConPrecios.get(j);
+            String[] datosDeLaLinea = lineaActual.split(",");
+            String[] datosDeLaLineaSiguiente = lineaSiguiente.split(",");
+            if(Double.parseDouble(datosDeLaLinea[2]) > Double.parseDouble(datosDeLaLineaSiguiente[2])){
+                fechaDato = datosDeLaLinea[0];
+                datoMax = Double.parseDouble(datosDeLaLinea[2]);
+            }else{
+                fechaDato = datosDeLaLineaSiguiente[0];
+                datoMax = Double.parseDouble(datosDeLaLineaSiguiente[2]);
+                i=j;
+            }
+            j++;
+        }
+        return new String[]{fechaDato,Double.toString(datoMax)};
+    }
+
+    public static String[] valorMin(List<String> lineasConPrecios){
+        int i=1;
+        int j=i+1;
+        double datoMin = 0;
+        String fechaDato = null;
+        while (i < lineasConPrecios.size() && j < lineasConPrecios.size()-1) {
+            String lineaActual = lineasConPrecios.get(i);
+            String lineaSiguiente = lineasConPrecios.get(j);
+            String[] datosDeLaLinea = lineaActual.split(",");
+            String[] datosDeLaLineaSiguiente = lineaSiguiente.split(",");
+            if(Double.parseDouble(datosDeLaLinea[3]) < Double.parseDouble(datosDeLaLineaSiguiente[3])){
+                fechaDato = datosDeLaLinea[0];
+                datoMin = Double.parseDouble(datosDeLaLinea[3]);
+            }else{
+                fechaDato = datosDeLaLineaSiguiente[0];
+                datoMin = Double.parseDouble(datosDeLaLineaSiguiente[3]);
+                i=j;
+            }
+            j++;
+        }
+        return new String[]{fechaDato,Double.toString(datoMin)};
     }
 }
+
